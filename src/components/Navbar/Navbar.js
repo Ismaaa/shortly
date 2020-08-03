b502c548529a575fb0df4511f7088a061f273ece
Isma <didouhismael@gmail.com>
2020-08-03 20:55:57 +0200

    Styled navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Navbar = () => <nav>Nav !!</nav>;
+const Navbar = () => (
+  <nav className="Navbar">
+    <img src="/assets/logo.svg" alt="Shortly" />
+    <div className="Navbar__burger">
+      <span className="Navbar__burger-slice" />
+      <span className="Navbar__burger-slice" />
+      <span className="Navbar__burger-slice" />
+    </div>
+  </nav>
+);
 
 export default Navbar;
