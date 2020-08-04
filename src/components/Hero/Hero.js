7a6825a0f8092599cd076d36486f0951f52967e2
Isma <didouhismael@gmail.com>
2020-08-04 22:59:29 +0200

    Navbar text and button styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Hero = () => (
   <div className="Hero">
-    <img
-      className="Hero__image"
-      src="/assets/illustration-working.svg"
-      alt=""
-      style={{ height: 300 }}
-    />
+    {/* <img */}
+    {/*  className="Hero__image" */}
+    {/*  src="/assets/illustration-working.svg" */}
+    {/*  alt="" */}
+    {/*  style={{ height: 300 }} */}
+    {/* /> */}
   </div>
 );
 
