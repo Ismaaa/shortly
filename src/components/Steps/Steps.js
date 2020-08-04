325eaa0f6d3b1342d03cc49b749bfa7e5e849d44
Isma <didouhismael@gmail.com>
2020-08-05 00:09:38 +0200

    added informative comment

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Steps = () => (
   <div className="Steps">
+    {/* card */}
     <h1 className="Title">Brand Recognition</h1>
     <p className="Text">
       Boost your brand recognition with each click. Generic links don't mean a
       thing. Branded links help instil confidence in your content.
     </p>
+    {/*  card */}
   </div>
 );
 
