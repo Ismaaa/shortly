4c7c9c3a118ae0b13f5bc556ebab4bd0dd882860
Isma <didouhismael@gmail.com>
2020-08-05 22:54:48 +0200

    setup reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import { logger } from 'redux-logger/src';
+import linksReducer from './ducks/links/index';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  links: linksReducer,
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
